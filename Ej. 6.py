154af0d5b24cf37b754ec4283d869571acce064f
Nerea López Astasio <al_nlopeza@iessierradelasvillas.org>
2022-10-13 10:15:21 +0200

    Intento ejercicio 6

---
+++ Ej. 6.py
@@ -0,0 +1,12 @@
+#Programa que lea una cadena por teclado y compruebe si es una letra mayúscula.
+from curses.ascii import isupper
+
+
+cadena=""
+
+cadena=input("Dime una cadena ")
+
+if cadena.islower()
+False
+>>> 'hola'.islower()
+True
